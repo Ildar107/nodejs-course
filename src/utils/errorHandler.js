4f62fb8b60c839353906ba8ccacfebfb88f08493
Ildar107 <ildar107wk@gmail.com>
2020-04-14 14:44:37 +0300

    fix: bug with settimeout error

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -3,10 +3,10 @@ const logger = require('../logger/winston');
 const errorHandler = (error) => {
     if(error.message !== undefined) {
         logger.error(error.message);
+        setTimeout(() => process.exit(1), 1000);
     } else {
         logger.error(error);
     }
-    process.exit(1);
 }
 
 module.exports = errorHandler;
